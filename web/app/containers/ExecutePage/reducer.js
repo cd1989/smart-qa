06cf0fff022ad50ba384631ecf8b8d1331f57289
cd1989 <chende@caicloud.io>
2018-12-22 12:17:04 +0800

    Don't set processing when load records

--- web/app/containers/ExecutePage/reducer.js
+++ web/app/containers/ExecutePage/reducer.js
@@ -23,11 +23,9 @@ export function reducer(state = initialState, action) {
         .set('error', action.error);
     case LOAD_RECORDS:
       return state
-        .set('processing', true)
         .set('error', false);
     case LOAD_RECORDS_SUCCEED:
       return state
-        .set('processing', false)
         .set('error', false)
         .set('data', action.data || []);
     default:
